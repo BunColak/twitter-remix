7b67ee076e68693173ead38c55600438d236428f
Bün Colak <bun.colak@motius.de>
2022-04-06 14:01:39 +0200

    role

--- app/routes/auth/index.tsx
+++ app/routes/auth/index.tsx
@@ -3,5 +3,6 @@ import { LoaderFunction } from "@remix-run/node";
 export const loader: LoaderFunction = async () => {
   return {
     "X-Hasura-User-Id": "1",
+    "X-Hasura-Role": "user"
   };
 };
